c013a34be786089ef325626da57e4636b53a5efa
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-09-21 14:21:26 +0530

    Update techgpt.py

--- techgpt.py
+++ techgpt.py
@@ -206,6 +206,8 @@ def run_model(query, model_app):
     model1 = SentenceTransformer('paraphrase-MiniLM-L3-v2')
     embedding1 = model1.encode("this is sentence").tolist()
 
+    model_app = 'Structured Text'
+
     if model_app == 'Structured Text':
         docs = db.similarity_search(query)
         # print("The answer based on Text matching search is \n", docs[0].page_content)
